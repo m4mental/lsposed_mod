0dea7519c6c4e069fc42118c62633b4c3895c09a
Sandesh kumar <96581098+m4mental@users.noreply.github.com>
2026-07-02 01:26:08 +0530

    Add Kotlin plugin and set Java 11 compatibility
    Added Kotlin Android plugin and updated compile options for Java 11.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   alias(libs.plugins.android.library)
+  alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -8,5 +9,14 @@ android {
 
   defaultConfig {
     minSdk = 24
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+
+  kotlinOptions {
+    jvmTarget = "11"
   }
 }
