fe3799c8c56513268deeb8dcd0c554b7a744b5d6
Sandesh kumar <96581098+m4mental@users.noreply.github.com>
2026-07-02 02:17:54 +0530

    Update project name and repository settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,12 @@
 pluginManagement {
   repositories {
-    google {
-      content {
-        includeGroupByRegex("com\\.android.*")
-        includeGroupByRegex("com\\.google.*")
-        includeGroupByRegex("androidx.*")
-      }
-    }
+    google()
     mavenCentral()
     gradlePluginPortal()
   }
 }
 
-plugins { 
-  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" 
-}
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
 
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
@@ -28,7 +20,7 @@ dependencyResolutionManagement {
   }
 }
 
-rootProject.name = "DynamicIslandLSPosed"
+rootProject.name = "My Application"
 
 include(":app")
 include(":api")
